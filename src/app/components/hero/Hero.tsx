aedb1ffdec805ca600e247ead7d070e23296271b
Agus9217 <justinibarrola92@gmail.com>
2024-10-01 13:43:39 -0300

    avanzando hero

--- src/app/components/hero/Hero.tsx
+++ src/app/components/hero/Hero.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react'
+import { Box, Flex, Heading, Text } from '@chakra-ui/react'
 import Image from 'next/image'
 import React from 'react'
 
@@ -8,16 +8,55 @@ export const Hero = () => {
       w={'100%'}
       h={'100dvh'}
     >
-      <Image
-        src={'/hero-imagen.jpg'}
-        alt={'Imagen de la iglesia'}
-        fill={true}
-        objectFit={'cover'}
-        objectPosition={'center'}
-        style={{
-          filter: 'brightness(40%)'
-        }}
-      />
+      <Box>
+        <Image
+          src={'/hero-imagen.jpg'}
+          alt={'Imagen de la iglesia'}
+          fill={true}
+          objectFit={'cover'}
+          objectPosition={'center'}
+          style={{
+            filter: 'brightness(40%)'
+          }}
+        />
+        <Flex
+          position={'absolute'}
+          top={'50%'}
+          left={'50%'}
+          transform={'translate(-50%, -50%)'}
+          flexDirection={'column'}
+          borderWidth={'thin'}
+          gap={8}
+          alignItems={'center'}
+
+          textAlign={'center'}
+        >
+          <Heading
+            as={'h2'}
+            textShadow={'xl'}
+            fontWeight={'600'}
+            textTransform={'uppercase'}
+            fontSize={'xl'}
+            letterSpacing={4}
+            color={'red.600'}
+          >
+            Bienvenidos se parte junto con nosotros
+          </Heading>
+          <Heading
+            color={'white'}
+            fontSize={'7xl'}
+          >
+            Iglesia Cristiana en Barrio Nuevo
+          </Heading>
+          <Text
+            maxW={'50%'}
+            w={'100%'}
+            color={'white'}
+          >
+            voluptatibus a libero optio sit dolores iure enim? Perferendis voluptatum quaerat sint ad voluptates, vitae, dolor doloremque odio nihil tempore eum.
+          </Text>
+        </Flex>
+      </Box>
     </Flex>
 
   )
